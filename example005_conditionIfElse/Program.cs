5862cf614f12e8c885f24a55215286a2ca9f9598
Jngvarr <jngvarr@gmail.com>
2022-10-13 17:46:41 +0700

    add example005_add condition if else

---
+++ example005_conditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+
+string userName=Console.ReadLine();
+if (userName == "Маша")
+{
+    Console.WriteLine("Ура!!! Это же МАША!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
